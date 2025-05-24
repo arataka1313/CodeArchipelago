e75aa8b253bcf6c41453c35c2e771a7eb3a0ed47
rintarotajima <rintarotajima@gmail.com>
2025-05-24 17:08:51 +0900

    fix: View.tsxファイルのエラー

--- frontend/src/pages/view/View.tsx
+++ frontend/src/pages/view/View.tsx
@@ -1,7 +1,5 @@
-import React from "react"
 import Header from "../../components/Header"
-import { useNavigate } from "react-router-dom";
-import CharacterRegistrationForm from "../../components/CharacterRegistrationForm
+import CharacterRegistrationForm from "../../components/CharacterRegistrationForm"
 
 export const View = () => {
     return (
