77a119056d876e7d776a5afd282ef5dde1fa3e9a
ryun1009 <ryunosuke130909@gmail.com>
2025-05-25 11:51:47 +0900

    フロントエンド側での相関図表示システム

--- frontend/src/components/Diagram.tsx
+++ frontend/src/components/Diagram.tsx
@@ -1,80 +1,24 @@
 import cytoscape from "cytoscape";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, } from "react";
+import { CreateElements } from "../utils/GetDiagram.tsx";
 
 
 function Diagram() {
-    const elems = [
-        // Nodes
-        {
-            data: {
-                id: "node-a",
-                label: "Node A"
-            },
-        },
-        {
-            data: {
-                id: "node-b",
-                label: "Node B"
-            }
-        },
-        {
-            data: {
-                id: "node-c",
-                label: "Node C",
-            }
-        },
-        {
-            data: {
-                id: "node-d",
-                label: "Node D"
-            }
-        },
-        {
-            data: {
-                id: "node-e",
-                label: "Node E"
-            }
-        },
-        // Edges
-        // color というカスタムプロパティを追加
-        {
-            data: {
-                id: "edge-a",
-                source: "node-a",
-                target: "node-b",
-                label: "Edge A",
-                color: "#ee827c"    // カスタムプロパティ
-            }
-        },
-        {
-            data: {
-                id: "edge-b",
-                source: "node-c",
-                target: "node-b",
-                label: "Edge B",
-                color: "#38b48b"    // カスタムプロパティ
-            }
-        },
-        {
-            data:{
-                id: "edge-c",
-                source: "node-e",
-                target: "node-d",
-                label: "Edge C",
-                color: "#89c3eb"    // カスタムプロパティ
-            }
-        }
-    ]
+    const elems = CreateElements();
 
     const styles = [
         {
             selector: "node",   // すべてのノードに適用
             style: {
-                width: "80px",
-                height: "80px",
+                width: "160px",
+                height: "160px",
                 label: "data(label)",   // このようにして data にアクセスすることで label を指示する
                 "border-width": 2,
-                "text-valign": "center" // ノードの縦方向に中心に
+                "text-valign": "bottom", // ノードの縦方向に中心に
+                "text-wrap": "wrap",
+                "text-max-width": "100px",
+                "shape": "round-rectangle",
+                "text-margin-y": -35
             } as const      // ts の場合は as const を付けないとエラーになる場合がある
         },
         {
